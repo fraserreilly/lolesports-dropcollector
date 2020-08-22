a325f8ce83ef7a6016dbe1c2a5efb1562789c661
Fraser Reilly <26841319+fraserreilly@users.noreply.github.com>
2020-08-22 20:05:52 +0100

    Fixed a few bugs and runs window headless.

--- dropcollector.py
+++ dropcollector.py
@@ -6,35 +6,45 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.firefox.options import Options
 import time
 import os
 
 HOMEPAGE_URL = "https://lolesports.com/"
-LIVE_URL = "https://lolesports.com/live"
+LIVE_URL1 = "https://lolesports.com/live/lec/lec"
+LIVE_URL2 = "https://lolesports.com/live/lcs/lcs"
 local_username = os.environ.get("LOLESPORTS_USERNAME")
 local_password = os.environ.get("LOLESPORTS_PASSWORD")
 
+# adds firefox options to allow us to run headless
+options = Options()
+options.headless = True
+
 # Open a Firefox browser window
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=options)
 
 # driver wait
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 86400)
 
 # Visit the login page
 driver.get(HOMEPAGE_URL)
 driver.find_element_by_xpath("//*[@data-riotbar-link-id='login']").click()
-time.sleep(5)
+wait.until(ec.title_is("Sign In"))
 
 # Gets username and password from local machine and inserts them
 driver.find_element_by_name("username").send_keys(local_username)
 driver.find_element_by_name("password").send_keys(local_password)
 driver.find_element_by_xpath('//*[@title="Sign In"]').click()
 
-# Wait until page is loaded before visiting the the live url
+# Wait until page is loaded before visiting the the LEC as they are first
 wait.until(ec.visibility_of_element_located((By.ID, "riotbar-account")))
-driver.get(LIVE_URL)
-time.sleep(30)
+driver.get(LIVE_URL1)
+wait.until(ec.url_changes("https://lolesports.com/live/lec/lec"))
 
-wait.until(ec.url_changes("https://lolesports.com/live/lec/lec" or "https://lolesports.com/live/lcs/lcs"))
+# Once LEC is done LCS will be the next stream so we load that stream
+driver.get(LIVE_URL2)
+wait.until(ec.url_changes("https://lolesports.com/live/lcs/lcs"))
+
+# Once LCS and LEC are both done we quit the driver and tell the user that all games are over.
 driver.quit()
 print("There are no live games with drops enabled.")
