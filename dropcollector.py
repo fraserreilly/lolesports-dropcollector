051629591b2c5e40bc012baf6d7e37b3dad723d3
Fraser Reilly <26841319+fraserreilly@users.noreply.github.com>
2020-08-23 20:48:43 +0100

    added some prints just to make sure everything works

--- dropcollector.py
+++ dropcollector.py
@@ -30,19 +30,23 @@ wait = WebDriverWait(driver, 86400)
 driver.get(HOMEPAGE_URL)
 driver.find_element_by_xpath("//*[@data-riotbar-link-id='login']").click()
 wait.until(ec.title_is("Sign In"))
+print("Going to the sign in page.")
 
 # Gets username and password from local machine and inserts them
 driver.find_element_by_name("username").send_keys(local_username)
 driver.find_element_by_name("password").send_keys(local_password)
 driver.find_element_by_xpath('//*[@title="Sign In"]').click()
+print("Logged into riot account.")
 
 # Wait until page is loaded before visiting the the LEC as they are first
 wait.until(ec.visibility_of_element_located((By.ID, "riotbar-account")))
 driver.get(LIVE_URL1)
+print("Watching LEC until it's finished.")
 wait.until(ec.url_changes("https://lolesports.com/live/lec/lec"))
 
 # Once LEC is done LCS will be the next stream so we load that stream
 driver.get(LIVE_URL2)
+print("Changed to LCS as LEC is over.")
 wait.until(ec.url_changes("https://lolesports.com/live/lcs/lcs"))
 
 # Once LCS and LEC are both done we quit the driver and tell the user that all games are over.
